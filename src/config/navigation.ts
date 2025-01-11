40b6dcecaf4de6bbb6dd9aedb164c773157912e8
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-01-11 21:43:17 +0400

    update

---
+++ src/config/navigation.ts
@@ -0,0 +1,31 @@
+export interface NavLink {
+    label: string;
+    href: string;
+}
+
+export const navigationLinks: NavLink[] = [
+    {
+        label: 'About',
+        href: '/about'
+    },
+    {
+        label: 'Projects',
+        href: '/projects'
+    },
+    {
+        label: 'Experience',
+        href: '/experience'
+    },
+    {
+        label: 'Education',
+        href: '/education'
+    },
+    {
+        label: 'Contact',
+        href: '/contact'
+    },
+    {
+        label: 'Webring',
+        href: '/webring'
+    }
+]; 
